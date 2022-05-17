d3a221e364c3a8b4684fe438a513e6f09a68bbbb
swiftproger <swiftapp@mail.ru>
2022-05-17 09:13:13 +0300

    test subdomain

--- WebService/main.py
+++ WebService/main.py
@@ -1,26 +1,18 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, subdomain_matching=True)
+app.config['SERVER_NAME'] = "swiftapp.ru:80"
 
 
 @app.route('/')
 def index():
-    return "swiftapp.ru _"
+    return "swiftapp.ru"
 
 
-@app.route("/", subdomain="static")
-def static_index():
-    """Flask supports static subdomains
-    This is available at static.your-domain.tld"""
-    return "static.your-domain.tld"
-
-
-@app.route("/dynamic", subdomain="<username>")
-def username_index(username):
-    """Dynamic subdomains are also supported
-    Try going to user1.your-domain.tld/dynamic"""
-    return username + ".your-domain.tld"
+@app.route("/", subdomain="api")
+def egg_index():
+    return "api.swiftapp.ru"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port='80', debug=True)
