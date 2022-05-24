f7efa69e2e1929f7f848afaded08a64b3bb11e68
swiftproger <swiftapp@mail.ru>
2022-05-24 19:05:40 +0300

    cerbot config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(swiftapp.ru www.swiftapp.ru)
+domains=(swiftapp.ru www.swiftapp.ru, api.swiftapp.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="swiftapp@gmail.ru" # Adding a valid address is strongly recommended
