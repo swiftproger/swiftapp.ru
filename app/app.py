41da4a511c780f3b4ce6cd84c726a5e4c3617dbf
swiftproger <swiftapp@mail.ru>
2022-05-25 09:59:19 +0300

    Редактирование Dockerfile

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Here be dragons!!!!!!"
+    return "swiftapp"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="80", debug=True)
+    # app.run(debug=True)
