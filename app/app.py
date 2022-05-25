e0f573f5309cbae2a97d2977913e0ccbb6105b89
swiftproger <swiftapp@mail.ru>
2022-05-25 09:06:07 +0300

    Тестирование запуска flask через gunicorn

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, Response
 
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
+@app.route("/")
+def index():
+    return "Here be dragons!!!!!!"
 
-    @app.route("/")
-    def index():
-        return "hello swiftapp"
-
-    return app
+if __name__ == "__main__":
+    app.run(debug=True)
