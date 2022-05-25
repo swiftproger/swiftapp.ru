81023790a1c9a704b9880afaa2f67535ca294564
swiftproger <swiftapp@mail.ru>
2022-05-25 22:02:43 +0300

    Правки конфига для запуска на сервере

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, Response
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 @application.route("/")
 def index():
@@ -9,4 +9,4 @@ def index():
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port="80", debug=True)
-    application.run(debug=False)
+    app.run(debug=False)
