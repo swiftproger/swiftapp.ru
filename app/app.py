8167cbdd51768675fabbf5ffcc33d26e111b49df
swiftproger <swiftapp@mail.ru>
2022-05-25 11:47:31 +0300

    Правки конфига для запуска на сервере

--- app/app.py
+++ app/app.py
@@ -7,5 +7,5 @@ def index():
     return "swiftapp"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="80", debug=True)
-    # app.run(debug=True)
+    # app.run(host="0.0.0.0", port="80", debug=True)
+    app.run(debug=False)
