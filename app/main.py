441b0db84586137df44c438eef478ee4a4f0bf1c
swiftproger <swiftapp@mail.ru>
2022-05-30 15:26:24 +0300

    Настройка flask приложения

--- app/main.py
+++ app/main.py
@@ -2,33 +2,18 @@ from flask import Flask
 
 app = Flask(__name__, subdomain_matching=True)
 
-app.config['SERVER_NAME'] = 'swiftapp.ru'
+app.config['SERVER_NAME'] = 'swiftapp.ru:80'
 app.url_map.default_subdomain = "www"
 
-#
-# @app.route("/")
-# def hello():
-#    return "<h1 style='color:blue'>Hello!!!</h1>"
+@app.route("/")
+def index():
+    return "example.com"
+
+@app.route("/", subdomain="api")
+def egg_index():
+    return "eggs.example.com"
 
 
-@app.route("/", subdomain="static")
-def static_index():
-    """Flask supports static subdomains
-    This is available at static.your-domain.tld"""
-    return "static.your-domain.tld"
-
-
-@app.route("/dynamic", subdomain="<username>")
-def username_index(username):
-    """Dynamic subdomains are also supported
-    Try going to user1.your-domain.tld/dynamic"""
-    return username + ".your-domain.tld"
-
-
-# @app.route('/', subdomain='<string:subdomain>', methods=['GET', 'POST'])
-# def handler(subdomain):
-
-   # return f"{subdomain}.swiftapp.ru!!"
 
 
 if __name__ == "__main__":
