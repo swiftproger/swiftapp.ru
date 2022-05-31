1ae440ea601264677a287706713b178b96011e7f
swiftproger <swiftapp@mail.ru>
2022-05-31 23:27:54 +0300

    Настройка flask приложения

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__, subdomain_matching=True)
-# app = Flask(__name__)
+# app = Flask(__name__, subdomain_matching=True)
+app = Flask(__name__)
 
 app.config['SERVER_NAME'] = '*.swiftapp.ru'
 app.url_map.default_subdomain = "www"
