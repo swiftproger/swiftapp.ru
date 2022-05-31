407d45173a6fb7259577a92d70b5b50310b710e5
swiftproger <swiftapp@mail.ru>
2022-05-31 23:01:50 +0300

    Настройка flask приложения

--- app/main.py
+++ app/main.py
@@ -1,27 +1,23 @@
 from flask import Flask
+from flask import Blueprint
 
-# app = Flask(__name__, subdomain_matching=True)
 app = Flask(__name__)
+app.config['SERVER_NAME'] = 'swiftapp.ru'
 
-app.config['SERVER_NAME'] = '*.swiftapp.ru'
-app.url_map.default_subdomain = "www"
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
+# Blueprint declaration
+bp = Blueprint('subdomain', __name__, subdomain="<url>")
 
-@app.route("/")
-def index():
-    return "swiftapp.ru"
+# Add a route to the blueprint
+@bp.route("/")
+def home(user):
+    return 'Welcome to your subdomain, {}'.format(user)
 
-@app.route("/user/<int:id>")
-def user(id):
-    return f"{id}.example.com"
+# Register the blueprint into the application
+app.register_blueprint(bp)
 
-
-@app.route("/d", subdomain="api")
-def egg_index():
-    return "api.example.com"
-
-
-
-
-if __name__ == "__main__":
-   app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True)
