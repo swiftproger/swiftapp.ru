35a9d6f49cb80b4c59ffd493adf3e81d18395b26
swiftproger <swiftapp@mail.ru>
2022-06-01 13:49:30 +0300

    ret_com

--- app/main.py
+++ app/main.py
@@ -3,24 +3,23 @@ from flask import Flask
 # app = Flask(__name__, subdomain_matching=True)
 app = Flask(__name__)
 
-# app.config['SERVER_NAME'] = '*.swiftapp.ru'
-# app.url_map.default_subdomain = "www"
+app.config['SERVER_NAME'] = '*.swiftapp.ru'
+app.url_map.default_subdomain = "www"
 
 
 @app.route("/")
 def index():
     return "swiftapp.ru !"
-#
-# @app.route("/user/<int:id>")
-# def user(id):
-#     return f"{id}.example.com"
-#
-#
-# @app.route("/dos", subdomain="api")
-# def egg_index():
-#     return "api.example.com"
-#
 
+
+@app.route("/user/<int:id>")
+def user(id):
+    return f"{id}.example.com"
+
+
+@app.route("/dos", subdomain="api")
+def egg_index():
+    return "api.example.com"
 
 
 if __name__ == "__main__":
