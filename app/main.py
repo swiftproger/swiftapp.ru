737ee1d9fc81b501be1337c349053c9cef5e5984
swiftproger <swiftapp@mail.ru>
2022-06-02 13:11:46 +0300

    Рабочая версия с поддоменами

--- app/main.py
+++ app/main.py
@@ -4,12 +4,15 @@ from flask import Blueprint
 app = Flask(__name__)
 app.config['SERVER_NAME'] = 'swiftapp.ru'
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 # Blueprint declaration
 bp = Blueprint('subdomain', __name__, subdomain="<user>")
+
 
 # Add a route to the blueprint
 @bp.route("/")
@@ -18,6 +21,7 @@ def home(user):
 
 # Register the blueprint into the application
 app.register_blueprint(bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
